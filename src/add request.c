da17f9308146458a23328829c4816d8e85f3aac2
Ajay <ajaykumar.88266@gmail.com>
2022-10-21 00:12:52 +0530

    newwww

--- src/add request.c
+++ src/add request.c
@@ -1,8 +1,34 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<macros.h>
 #include<struct.h>
 
-void addRequest()
-{
+void addRequest(){
+    //FILE *fp; 
+	//fp = fopen("Customer.txt","wb+");
+
+    request r;
+    printf("\nEnter Request Details : ");
+   	printf("\nCustomer Id: ");
+	scanf("%d",&c.custID); 
+	printf("\nMobile Number : ");
+	scanf("%ld",&c.phoneNum);
+	getchar( );
+	printf("\nFirst Name : ");
+	fgets(c.firstName,SIZE,stdin);
+	c.firstName[strlen(c.firstName)-1] = '\0';
+	getchar( );
+	printf("\nLast Lame : ");
+	fgets(c.lastName,SIZE,stdin);
+	c.lastName[strlen(c.lastName)-1] = '\0';
+	getchar( );
+	printf("\nPermanent Address :");
+	fgets(c.address,SIZE,stdin);
+	c.address[strlen(c.address)-1] = '\0';
+	getchar( );
+	printf("\nCustomer Type (New/Existing) : ");
+	fgets(c.custType,SIZE,stdin);
+	c.custType[strlen(c.custType)-1] = '\0';
 
     
 }

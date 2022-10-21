cb1524902d23159cdd10f3dade98802455184878
Ajay <ajaykumar.88266@gmail.com>
2022-10-21 10:12:32 +0530

    a

--- src/addRequest.c
+++ src/addRequest.c
@@ -3,6 +3,23 @@
 
 
 void addRequest(){
+
+    request r;
+    while (1)
+    {    
+        printf("\nRequest Date:");
+        scanf("%d%d%d",&r.requestDate.day,&r.requestDate.month,&r.requestDate.year);
+        if(isValid(r.requestDate))
+            break;
+        else
+            printf("Invalid Date...Enter again");
+        
+            
+    }
+
+            
+
+   
    
     
 }

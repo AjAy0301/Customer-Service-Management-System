6f50e412f6f55c342ee72ebce670fe2a9e0ba8fe
Ajay <ajaykumar.88266@gmail.com>
2022-10-21 10:15:16 +0530

    yes

--- src/addCustomer.c
+++ src/addCustomer.c
@@ -2,12 +2,8 @@
 #include<struct.h>
 #include<functions.h>
 
-<<<<<<< HEAD:src/add customer.c
-void main(){
-=======
 void addCustomer(){
 
->>>>>>> b8f34eb4da5dce90343756c30525d1a66b81dfb1:src/addCustomer.c
     customer c;
     printf("\nEnter Customer Details : ");
 
@@ -32,7 +28,7 @@ void addCustomer(){
 	fgets(c.address,SIZE,stdin);
 	c.address[strlen(c.address)-1] = '\0';
 	getchar( );
-	
+
 	printf("\nCustomer Type (New/Existing) : ");
 	fgets(c.custType,SIZE,stdin);
 	c.custType[strlen(c.custType)-1] = '\0';
